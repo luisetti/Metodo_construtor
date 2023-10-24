a2c1cadd6642bf9b525f0e539463e49185eb14fd
luisetti <luis.s.silvafernando@gmail.com>
2023-10-24 14:33:42 +0000

    salvo

--- Principal.java
+++ Principal.java
@@ -1,5 +1,11 @@
 public class Principal {
     public static void main(String args){
-        
+       
+        Carro car = new Carro();
+        Carro carAro = new Carro(14);
+        Carro carTresParametros= new
+        Carro(14,"HB 20 2021 sense", 60000);
+        Carro carQuatroParametros = 
+        new Carro("HB 20 2021 sense", 14, 60000, "branco");
     }
 }
