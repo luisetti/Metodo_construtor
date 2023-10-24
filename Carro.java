6bf13687035a461c30d72977a7bbf7b25b13ffd3
luisetti <luis.s.silvafernando@gmail.com>
2023-10-24 15:02:35 +0000

    salvo

--- Carro.java
+++ Carro.java
@@ -4,6 +4,31 @@ public class Carro{
     private int aro;
     private double valor;
 
+    public int getAro(){
+        return aro;
+    }
+    public void setAro(int aro){
+        this.aro = aro;
+    }
+    public String getCor(){
+        return cor;
+    }
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public double getValor(){
+        return valor;
+    }
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+
     public Carro(){
     }
     public Carro(int aro){
@@ -17,5 +42,11 @@ public class Carro{
         this.aro=aro;
         this.modelo=modelo;
         this.valor=valor;
+    }
+    public Carro(int aro, String modelo, double valor,String cor){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valor=valor;
+        this.cor=cor;
     }
 }
--- Principal.java
+++ Principal.java
@@ -3,9 +3,14 @@ public class Principal {
        
         Carro car = new Carro();
         Carro carAro = new Carro(14);
-        Carro carTresParametros= new
+        Carro carParametros= new
         Carro(14,"HB 20 2021 sense", 60000);
         Carro carQuatroParametros = 
-        new Carro("HB 20 2021 sense", 14, 60000, "branco");
+        new Carro(14,"Hb 20 2021 sense", 60000, "branco");
+
+        System.out.println(carAro.getAro());
+        System.out.println(carQuatroParametros.getCor());
+        System.out.println(carParametros.getValor());
+        System.out.println(car);
     }
 }
